f86f6ef9dcd127efcd172a70ef7f77c493f3aff6
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2025-12-01 23:34:23 +0900

    초기설정
    프로젝트 버전 : JAVA 21 / Spring 4.0.0
    
    들어간 초기 기술은 :
    
    *  Spring Web
    *  Spring Security
    *  Spring Validation
    *  Spring JPA
    *  OAuth2 Client
    *  Validation
    *  DevTools
    *  Lombok
    *  H2

---
+++ ma-recurit/src/main/java/com/ma_recurit/MaRecuritApplication.java
@@ -0,0 +1,13 @@
+package com.ma_recurit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaRecuritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaRecuritApplication.class, args);
+	}
+
+}
---
+++ ma-recurit/src/test/java/com/ma_recurit/MaRecuritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma_recurit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaRecuritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
