d7f4a1b4ef66126745b929ff97b6fc25813c610f
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2025-12-06 02:22:05 +0900

    Delete ma-recurit directory

--- ma-recurit/src/main/java/com/ma_recurit/entity/member/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ma_recurit.entity.member;
-
-public enum Role {
-    ADMIN, USER
-}
--- ma-recurit/src/main/java/com/ma_recurit/repository/party/PartyPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ma_recurit.repository.party;
-
-import com.ma_recurit.entity.party.PartyPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface PartyPostRepository extends JpaRepository<PartyPost, BigInteger> {
-}
